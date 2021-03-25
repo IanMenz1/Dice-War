0e1657d9bf3ea24832654a4eacf5cac0245bdf37
Ian Menz <65043537+IanMenz1@users.noreply.github.com>
2021-03-25 07:40:09 +0000

    Update LoadedDie.java

--- LoadedDie.java
+++ LoadedDie.java
@@ -1,10 +1,10 @@
 public class LoadedDie extends Die {
-    public LoadedDie(int diceSide1) {
-        super(diceSide1);
+    public LoadedDie(int n) {
+        super(n);
     }
 
     public int randNum() {
-        int randNum = (int) (Math.random()* diceSide1 + 2) - 1;
+        int randNum = (int) (Math.random()* n + 2) - 1;
         return randNum;
     }
 }
